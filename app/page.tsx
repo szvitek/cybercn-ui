6cba0fa6cb8923b579999372ad70ad170377fee2
Gyula Szvitek <gy.szvitek@gmail.com>
2026-03-29 19:21:00 +0100

    feat(Theme): migrate list styles to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -89,47 +89,47 @@ export default function Home() {
           drug culture, technology, and the sexual revolution while avoiding the
           utopian tendencies of earlier science fiction.
         </Paragraph>
-        <List Tag="ul">
-          <LI>
+        <List variant="ul">
+          <List.Item>
             Cyberpunk is a subgenre of science fiction in a dystopian{" "}
             <a href="#">futuristic setting that</a> tends to focus on a
             &ldquo;combination of low-life and high tech&rdquo;
-          </LI>
-          <LI glitched>
+          </List.Item>
+          <List.Item glitched>
             featuring advanced technological and scientific achievements, such
             as artificial intelligence and cybernetics, juxtaposed with a degree
             of breakdown or radical change in the social order.
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             Much of cyberpunk is rooted in the New Wave science fiction movement
             of the 1960s and 1970s, when writers like Philip K. Dick, Roger
             Zelazny, John Brunner, J. G. Ballard, Philip José Farmer and Harlan
             Ellison examined the impact of drug culture, technology,
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             and the sexual revolution while avoiding the utopian tendencies of
             earlier science fiction.
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             Cyberpunk is a subgenre of science fiction in a dystopian futuristic
             setting that tends to focus on a &ldquo;combination of low-life and
             high tech&rdquo;
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             featuring advanced technological and scientific achievements, such
             as artificial intelligence and cybernetics, juxtaposed with a degree
             of breakdown or radical change in the social order.
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             Much of cyberpunk is rooted in the New Wave science fiction movement
             of the 1960s and 1970s, when writers like Philip K. Dick, Roger
             Zelazny, John Brunner, J. G. Ballard, Philip José Farmer and Harlan
             Ellison examined the impact of drug culture, technology,
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             and the sexual revolution while avoiding the utopian tendencies of
             earlier science fiction.
-          </LI>
+          </List.Item>
         </List>
         <Paragraph inverse scanned="vertical">
           Cyberpunk is a subgenre of science fiction in a dystopian{" "}
@@ -157,47 +157,47 @@ export default function Home() {
           drug culture, technology, and the sexual revolution while avoiding the
           utopian tendencies of earlier science fiction.
         </Paragraph>
-        <List Tag="ol">
-          <LI>
+        <List variant="ol">
+          <List.Item>
             Cyberpunk is a subgenre of science fiction in a dystopian{" "}
             <a href="#">futuristic setting that</a> tends to focus on a
             &ldquo;combination of low-life and high tech&rdquo;
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             featuring advanced technological and scientific achievements, such
             as artificial intelligence and cybernetics, juxtaposed with a degree
             of breakdown or radical change in the social order.
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             Much of cyberpunk is rooted in the New Wave science fiction movement
             of the 1960s and 1970s, when writers like Philip K. Dick, Roger
             Zelazny, John Brunner, J. G. Ballard, Philip José Farmer and Harlan
             Ellison examined the impact of drug culture, technology,
-          </LI>
-          <LI glitched>
+          </List.Item>
+          <List.Item glitched>
             and the sexual revolution while avoiding the utopian tendencies of
             earlier science fiction.
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             Cyberpunk is a subgenre of science fiction in a dystopian futuristic
             setting that tends to focus on a &ldquo;combination of low-life and
             high tech&rdquo;
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             featuring advanced technological and scientific achievements, such
             as artificial intelligence and cybernetics, juxtaposed with a degree
             of breakdown or radical change in the social order.
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             Much of cyberpunk is rooted in the New Wave science fiction movement
             of the 1960s and 1970s, when writers like Philip K. Dick, Roger
             Zelazny, John Brunner, J. G. Ballard, Philip José Farmer and Harlan
             Ellison examined the impact of drug culture, technology,
-          </LI>
-          <LI>
+          </List.Item>
+          <List.Item>
             and the sexual revolution while avoiding the utopian tendencies of
             earlier science fiction.
-          </LI>
+          </List.Item>
         </List>
       </Section>
 
--- components/ui/cybercn/list.tsx
+++ components/ui/cybercn/list.tsx
@@ -1,14 +1,70 @@
 import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
 
-type ListProps = React.PropsWithChildren<{
-  Tag: "ul" | "ol";
-  className?: string;
-}>;
+const ListVariants = cva("mbs-5 mbe-5 ps-10", {
+  variants: {
+    variant: {
+      ul: "cyber-ul",
+      ol: "cyber-ol",
+    },
+  },
+  defaultVariants: {
+    variant: "ul",
+  },
+});
 
-export function List({ children, className, Tag, ...props }: ListProps) {
+const ListItemVariants = cva("cyber-li", {
+  variants: {
+    glitched: {
+      false: null,
+      true: "animate-li-glitched",
+    },
+  },
+  defaultVariants: {
+    glitched: false,
+  },
+});
+
+type ListVariantProps = VariantProps<typeof ListVariants>;
+
+export type ListProps = React.HTMLAttributes<
+  HTMLOListElement | HTMLUListElement
+> &
+  ListVariantProps & {
+    tag?: "ul" | "ol";
+  };
+
+type ListItemVariantProps = VariantProps<typeof ListItemVariants>;
+
+export type ListItemProps = ListItemVariantProps &
+  React.LiHTMLAttributes<HTMLLIElement>;
+
+export function List({
+  children,
+  className,
+  variant = "ul",
+  tag,
+  ...props
+}: ListProps) {
+  const Tag = tag || (variant as "ul" | "ol");
   return (
-    <Tag className={cn("cyberpunk mbs-5 mbe-5 ps-10", className)} {...props}>
+    <Tag className={cn(ListVariants({ variant }), className)} {...props}>
       {children}
     </Tag>
   );
 }
+
+export function ListItem({
+  children,
+  className,
+  glitched,
+  ...props
+}: ListItemProps) {
+  return (
+    <li className={cn(ListItemVariants({ glitched }), className)} {...props}>
+      {children}
+    </li>
+  );
+}
+
+List.Item = ListItem;
